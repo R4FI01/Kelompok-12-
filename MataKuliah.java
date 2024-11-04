0f4f6fe26ae1f902b16079d35c20ebf7d6b2146d
R4FI01 <rafiladoangin@gmail.com>
2024-11-04 12:55:58 +0700

    Update MataKuliah.java

--- MataKuliah.java
+++ MataKuliah.java
@@ -1,9 +1,1 @@
-public class MataKuliah {
-        String Nama;
-        int sks;
 
-        public MataKuliah (String Nama, int sks ){
-            this.Nama = Nama;
-            this.sks = sks;
-        }
-}
